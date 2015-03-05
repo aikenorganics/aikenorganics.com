26781fa727193a26444535303da6eea23d03f5d5
Brad Dunbar <dunbarb2@gmail.com>
2015-03-04 21:12:01 -0500

    Supplier name.

---
+++ migrations/20150304211058-supplier-name.js
@@ -0,0 +1,11 @@
+"use strict";
+
+module.exports = {
+  up: function(migration, DataTypes, done) {
+    migration.addColumn('suppliers', 'name', {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: ''
+    }).complete(done);
+  }
+};
