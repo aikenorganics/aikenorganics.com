6d3bc8eccb8601840c2b2ccc5b0b136f2dba98d0
Brad Dunbar <dunbarb2@gmail.com>
2015-03-07 07:53:52 -0500

    Product category id.

---
+++ migrations/20150307074944-product-category-id.js
@@ -0,0 +1,13 @@
+"use strict";
+
+module.exports = {
+  up: function(migration, DataTypes, done) {
+    migration.addColumn('products', 'category_id', {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 1,
+      references: 'categories',
+      referencesKey: 'id'
+    }).complete(done);
+  }
+};
