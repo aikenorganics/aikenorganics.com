98ee561da91507ad4d0180c8b944064a8edc322e
Brad Dunbar <dunbarb2@gmail.com>
2015-03-14 17:48:04 -0400

    Authorize products as well.

--- mid/growers/authorize.js
+++ mid/growers/authorize.js
@@ -1,17 +1,19 @@
 var models = require('../../models')
 
 module.exports = function (req, res, next) {
-  if (!req.user || !req.grower) return next()
+  if (!req.user || !req.grower && !req.product) return next()
 
   if (req.admin) {
     req.canEdit = res.locals.canEdit = true
     return next()
   }
 
+  var grower_id = req.grower ? req.grower.id : req.product.grower_id
+
   models.UserGrower.findOne({
     where: {
       user_id: req.user.id,
-      grower_id: req.grower.id
+      grower_id: grower_id
     }
   }).then(function (userGrower) {
     req.canEdit = res.locals.canEdit = !!userGrower
--- test/mid/growers/authorize.js
+++ test/mid/growers/authorize.js
@@ -2,8 +2,18 @@ var test = require('tape')
 var models = require('../../../models')
 var authorize = require('../../../mid/growers/authorize')
 
-test('growers/authorize: missing user and grower', function (t) {
+test('growers/authorize: missing user and product/grower', function (t) {
   var req = {}
+  var res = {locals: {}}
+  authorize(req, res, function () {
+    t.ok(!req.canEdit)
+    t.ok(!res.locals.canEdit)
+    t.end()
+  })
+})
+
+test('growers/authorize: missing user', function (t) {
+  var req = {product: {grower_id: 1}}
   var res = {locals: {}}
   authorize(req, res, function () {
     t.ok(!req.canEdit)
@@ -33,6 +43,16 @@ test('growers/authorize: missing grower', function (t) {
 })
 
 test('growers/authorize: admin', function (t) {
+  var req = {user: {id: 1}, product: {grower_id: 1}, admin: 1}
+  var res = {locals: {}}
+  authorize(req, res, function () {
+    t.ok(req.canEdit)
+    t.ok(res.locals.canEdit)
+    t.end()
+  })
+})
+
+test('growers/authorize: admin', function (t) {
   var req = {user: {id: 1}, grower: {id: 1}, admin: 1}
   var res = {locals: {}}
   authorize(req, res, function () {
@@ -43,11 +63,31 @@ test('growers/authorize: admin', function (t) {
 })
 
 test('growers/authorize: regular user', function (t) {
+  var req = {user: {id: 2}, product: {grower_id: 1}}
+  var res = {locals: {}}
+  authorize(req, res, function () {
+    t.ok(!req.canEdit)
+    t.ok(!res.locals.canEdit)
+    t.end()
+  })
+})
+
+test('growers/authorize: regular user', function (t) {
   var req = {user: {id: 2}, grower: {id: 1}}
   var res = {locals: {}}
   authorize(req, res, function () {
     t.ok(!req.canEdit)
     t.ok(!res.locals.canEdit)
+    t.end()
+  })
+})
+
+test('growers/authorize: authorized user', function (t) {
+  var req = {user: {id: 5}, product: {grower_id: 1}}
+  var res = {locals: {}}
+  authorize(req, res, function () {
+    t.ok(req.canEdit)
+    t.ok(res.locals.canEdit)
     t.end()
   })
 })
@@ -64,6 +104,16 @@ test('growers/authorize: authorized user', function (t) {
 
 test('growers/authorize: authorized user for different grower', function (t) {
   var req = {user: {id: 5}, grower: {id: 2}}
+  var res = {locals: {}}
+  authorize(req, res, function () {
+    t.ok(!req.canEdit)
+    t.ok(!res.locals.canEdit)
+    t.end()
+  })
+})
+
+test('growers/authorize: authorized user for different grower', function (t) {
+  var req = {user: {id: 5}, product: {grower_id: 2}}
   var res = {locals: {}}
   authorize(req, res, function () {
     t.ok(!req.canEdit)
