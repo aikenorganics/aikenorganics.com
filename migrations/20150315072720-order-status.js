0b92f37be955f5efc507040fabddedbbb87791d8
Brad Dunbar <dunbarb2@gmail.com>
2015-03-15 07:36:10 -0400

    Order status migrations.

---
+++ migrations/20150315072720-order-status.js
@@ -0,0 +1,9 @@
+module.exports = {
+  up: function (migration, DataTypes, done) {
+    migration.sequelize.query(`
+      create type order_status as enum ('open', 'complete', 'canceled');
+    `).then(function () {
+      done()
+    })
+  }
+}
---
+++ migrations/20150315073140-order-status-column.js
@@ -0,0 +1,11 @@
+module.exports = {
+  up: function (migration, DataTypes, done) {
+    migration.sequelize.query(`
+      alter table orders
+      add column status order_status
+      not null default 'open';
+    `).then(function () {
+      done()
+    })
+  }
+}
