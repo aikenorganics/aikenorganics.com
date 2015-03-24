a4eb1c10bfcb5995ab64470d6bcb35ae40cee1d7
Brad Dunbar <dunbarb2@gmail.com>
2015-03-24 06:30:30 -0400

    Call next!

--- mid/secure.js
+++ mid/secure.js
@@ -1,6 +1,5 @@
 module.exports = function (req, res, next) {
   var protocol = req.headers['x-forwarded-proto']
-  if (protocol !== 'https') {
-    res.redirect(`https://${req.hostname}${req.originalUrl}`)
-  }
+  if (protocol === 'https') return next()
+  res.redirect(`https://${req.hostname}${req.originalUrl}`)
 }
