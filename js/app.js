4c1b7d231f928075bb1d6a82c23db7494cf64499
Brad Dunbar <dunbarb2@gmail.com>
2015-07-13 06:23:11 -0400

    Prevent double submission.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 var $ = window.jQuery = require('jquery')
 require('bootstrap')
 
-setTimeout(function() {
+var messageTimeout = setTimeout(function() {
   $('#message').removeClass('active')
 }, 10000)
 
@@ -9,4 +9,12 @@ $(document).on('click', '[data-confirm]', function (e) {
   if (!confirm($(e.target).data('confirm'))) {
     e.preventDefault()
   }
+})
+
+$(document).on('submit', 'form', function (e) {
+  $(e.target).find(':button,:submit').prop('disabled', true)
+  clearTimeout(messageTimeout)
+  $('#message').html(
+    "<span class='alert alert-info'>Working…</span>"
+  ).addClass('active')
 })
