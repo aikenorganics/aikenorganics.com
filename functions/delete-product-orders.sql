95f7fccdf19205279ab8963e74676b4da35a7c66
Brad Dunbar <dunbarb2@gmail.com>
2015-07-18 14:01:17 -0400

    Remove functions directory.

--- functions/delete-product-orders.sql
+++
@@ -1,9 +0,0 @@
-create or replace function delete_product_orders()
-returns trigger
-as $$
-begin
-  delete from product_orders
-  where order_id = OLD.id;
-  return OLD;
-end;
-$$ language plpgsql;
--- functions/set-product-order-cost.sql
+++
@@ -1,6 +0,0 @@
-create or replace function set_product_order_cost() returns trigger as $$
-begin
-  NEW.cost := (select cost from products where id = NEW.product_id);
-  return NEW;
-end;
-$$ language plpgsql;
