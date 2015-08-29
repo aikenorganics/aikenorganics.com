3b56811af641f97433da8dbcd78ceb248714d802
Brad Dunbar <dunbarb2@gmail.com>
2015-08-29 12:38:14 -0400

    No define.

--- db/location.js
+++ db/location.js
@@ -1,22 +1,35 @@
 'use strict'
 
-module.exports = require('./db').define({
+let db = require('./db')
 
-  tableName: 'locations',
+class Location extends db.Model {
 
-  columns: [
-    {name: 'id'},
-    {name: 'name'},
-    {name: 'created_at', property: 'createdAt'},
-    {name: 'updated_at', property: 'updatedAt'}
-  ],
+  static get tableName () {
+    return 'locations'
+  }
+
+  static get columns () {
+    return [
+      {name: 'id'},
+      {name: 'name'},
+      {name: 'created_at', property: 'createdAt'},
+      {name: 'updated_at', property: 'updatedAt'}
+    ]
+  }
 
   get name () {
     return this.data.get('name') || ''
-  },
+  }
 
   set name (value) {
     this.data.set('name', value || '')
   }
 
-})
+  update (values) {
+    values.updatedAt = new Date
+    return super.update(values)
+  }
+
+}
+
+module.exports = Location
