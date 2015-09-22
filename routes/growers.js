083dc524cf6ab3c649e9d8c382cd13a8c62f03b3
Brad Dunbar <dunbarb2@gmail.com>
2015-09-22 08:10:53 -0400

    Growers!

--- routes/growers.js
+++ routes/growers.js
@@ -1,23 +1,21 @@
-var ozymandias = require('ozymandias')
-var upload = require('../mid/image-upload')
-var models = require('../models')
-var router = module.exports = ozymandias.Router()
+'use strict'
+
+let db = require('../db')
+let find = require('../mid/_find')
+let upload = require('../mid/image-upload')
+let ozymandias = require('ozymandias')
+let router = module.exports = ozymandias.Router()
 
 // Find
-router.param('grower_id', require('../mid/find')(models.Grower))
+router.param('grower_id', find('grower', function () { return db.Grower }))
 
 // Authorize
 router.param('grower_id', require('../mid/growers/authorize'))
 
 // Index
 router.get('/', function (req, res) {
-  models.Grower.findAll({
-    where: {active: true},
-    order: [['name', 'ASC']]
-  }).then(function (growers) {
-    res.render('growers/index', {
-      growers: growers
-    })
+  db.Grower.where({active: true}).order('name').all().then(function (growers) {
+    res.render('growers/index', {growers: growers})
   })
 })
 
@@ -30,32 +28,26 @@ router.get('/new', function (req, res) {
 router.post('/', function (req, res) {
   if (!req.admin) return res.status(401).render('401')
 
-  req.transaction(function (transaction) {
-    return models.Grower.create(req.body, {
-      fields: ['url', 'name', 'email', 'location', 'description'],
-      transaction: transaction
-    }).then(function (grower) {
+  db.transaction(function () {
+    db.Grower.create(req.permit(
+      'url', 'name', 'email', 'location', 'description'
+    )).then(function (grower) {
       res.flash('success', 'Saved')
       res.redirect(`/growers/${grower.id}`)
     })
-  })
+  }).catch(res.error)
 })
 
 // Show
 router.get('/:grower_id', function (req, res) {
-  var where = {}
-  if (!req.canEdit) where.active = true
+  let products = db.Product.where({grower_id: req.grower.id})
+  if (!req.canEdit) products.where({active: true})
 
-  req.grower.getProducts({
-    where: where,
-    order: [['name', 'ASC']]
-  }).then(function (products) {
+  products.order('name').all().then(function (products) {
     // Stupid, but necessary.
-    products.forEach(function (product) { product.grower = req.grower })
-    res.render('growers/show', {
-      products: products
-    })
-  })
+    for (let product of products) product.grower = req.grower
+    res.render('growers/show', {products: products})
+  }).catch(res.error)
 })
 
 // Edit Grower
@@ -67,15 +59,14 @@ router.get('/:grower_id/edit', function (req, res) {
 router.post('/:grower_id', function (req, res) {
   if (!req.canEdit) return res.status(401).render('401')
 
-  req.transaction(function (transaction) {
-    return req.grower.update(req.body, {
-      fields: ['name', 'email', 'url', 'location', 'description'],
-      transaction: transaction
-    }).then(function () {
-      res.flash('success', 'Saved')
-      res.redirect('/growers/' + req.grower.id)
-    })
-  })
+  db.transaction(function () {
+    req.grower.update(req.permit(
+      'name', 'email', 'url', 'location', 'description'
+    ))
+  }).then(function () {
+    res.flash('success', 'Saved')
+    res.redirect(`/growers/${req.grower.id}`)
+  }).catch(res.error)
 })
 
 // New Product
@@ -84,7 +75,7 @@ router.get('/:grower_id/products/new', newProduct)
 function newProduct (req, res) {
   if (!req.canEdit) return res.status(401).render('401')
 
-  models.Category.findAll().then(function (categories) {
+  db.Category.all().then(function (categories) {
     res.render('products/new', {categories: categories})
   })
 }
@@ -92,46 +83,42 @@ function newProduct (req, res) {
 router.post('/:grower_id/products', function (req, res) {
   if (!req.canEdit) return res.status(401).render('401')
 
-  var product = models.Product.build(req.body, {
-    attributes: [
-      'name', 'cost', 'unit', 'supply', 'category_id', 'description'
-    ]
-  })
-  product.grower_id = req.grower.id
+  let props = req.permit(
+    'name', 'cost', 'unit', 'supply', 'category_id', 'description'
+  )
+  props.grower_id = req.grower.id
 
-  req.transaction(function (transaction) {
-    return product.save({
-      transaction: transaction
-    }).then(function () {
+  db.transaction(function () {
+    db.Product.create(props).then(function (product) {
       res.flash('success', 'Saved')
       res.redirect(`/products/${product.id}`)
-    }).catch(function (error) {
-      res.status(422)
-      res.locals.error = error
-      res.locals.product = product
-      newProduct(req, res)
     })
-  })
+  }).catch(function (e) {
+    if (e.message !== 'invalid') throw e
+    res.status(422)
+    res.locals.errors = e.model.errors
+    res.locals.product = e.model
+    newProduct(req, res)
+  }).catch(res.error)
 })
 
 // Orders
 router.get('/:grower_id/orders', function (req, res) {
   if (!req.canEdit) return res.status(401).render('401')
 
-  var attrs = []
-  for (var attr in models.Product.attributes) attrs.push(attr)
-  attrs.push([`(
+  let total = `(
     select sum(quantity * cost) from product_orders
     inner join orders on orders.id = product_orders.order_id
     where product_id = products.id and orders.status = 'open'
-  )`, 'total'])
+  )::int as total`
 
-  req.grower.getProducts({
-    attributes: attrs,
-    where: {reserved: {gt: 0}}
-  }).then(function (products) {
+  db.Product
+  .select('*', total)
+  .where({grower_id: req.grower.id})
+  .where('reserved > 0')
+  .all().then(function (products) {
     res.render('growers/orders', {products: products})
-  })
+  }).catch(res.error)
 })
 
 router.post('/:grower_id/image', upload('grower'))
