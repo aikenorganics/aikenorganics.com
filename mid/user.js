615fdc81e4f6756f3960572d9ff5e95fb13b214a
Brad Dunbar <dunbarb2@gmail.com>
2015-09-26 15:22:01 -0400

    req.user -> db.User

--- mid/user.js
+++ mid/user.js
@@ -1,12 +1,14 @@
-var User = require('../models').User
+'use strict'
+
+let db = require('../db')
 
 module.exports = function (req, res, next) {
-  var id = req.session.userId
+  let id = req.session.userId
   if (!id) return next()
-  User.find(id).then(function (user) {
+  db.User.find(id).then(function (user) {
     if (!user) return next()
     req.user = res.locals.user = user
     req.admin = res.locals.admin = user.is_admin
     next()
-  })
+  }).catch(res.error)
 }
