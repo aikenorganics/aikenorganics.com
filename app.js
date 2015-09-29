efe58dcd974e7849aa83bd32c6ead9daa9a47400
Brad Dunbar <dunbarb2@gmail.com>
2015-09-28 21:57:07 -0400

    Proxying assets does not require auth.

--- app.js
+++ app.js
@@ -8,8 +8,13 @@ var session = require('cookie-session')
 var app = module.exports = ozymandias()
 app.locals = require('./helpers')
 
-// Middleware
+// Ensure requests are secure.
 if (app.get('env') === 'production') app.use(require('./mid/secure'))
+
+// Proxy assets from S3
+app.use('/assets', require('./routes/assets'))
+
+// Middleware
 app.use(session({
   signed: app.get('env') === 'production',
   name: 'aikenorganics',
@@ -28,7 +33,6 @@ app.use(require('./mid/flash'))
 app.use('/auth', require('./routes/auth'))
 app.use('/cart', require('./routes/cart'))
 app.use('/admin', require('./routes/admin'))
-app.use('/assets', require('./routes/assets'))
 app.use('/orders', require('./routes/orders'))
 app.use('/growers', require('./routes/growers'))
 app.use('/products', require('./routes/products'))
