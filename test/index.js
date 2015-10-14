f490bf7633f409eb42de036b0c4e593f7d2ee380
Brad Dunbar <dunbarb2@gmail.com>
2015-10-13 21:42:10 -0400

    Cleanup.

--- test/index.js
+++ test/index.js
@@ -1,12 +1,14 @@
-var test = require('tape')
-var db = require('../db')
+'use strict'
+
+let test = require('tape')
+let db = require('../db')
 
 require('./db')
 require('./lib')
 require('./mid')
 require('./routes')
 
-test('teardown', function (t) {
+test('teardown', (t) => {
   db.close()
   t.end()
 })
