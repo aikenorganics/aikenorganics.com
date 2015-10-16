45d117c21b0ca5e83d560d4f3e2052d17cb9ce70
Brad Dunbar <dunbarb2@gmail.com>
2015-10-15 21:42:44 -0400

    Order products by name.

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -7,7 +7,7 @@ let router = module.exports = ozymandias.Router()
 router.get('/', function (req, res) {
   let query = db.Product.include('grower')
   if (req.query.oversold === '1') query.where('reserved > supply')
-  query.all().then(function (products) {
+  query.order('name').all().then(function (products) {
     res.render('admin/products/index', {products: products})
   }).catch(res.error)
 })
