ba22ace89b96f605a2803529cc87d201629f5375
Brad Dunbar <dunbarb2@gmail.com>
2015-11-06 08:07:42 -0500

    Uploads with promises.

--- mid/image-upload.js
+++ mid/image-upload.js
@@ -1,119 +1,93 @@
 'use strict'
 
-let fs = require('fs')
-let gm = require('gm')
-let aws = require('aws-sdk')
-let async = require('async')
-let mkdirp = require('mkdirp')
+const fs = require('fs')
+const gm = require('gm')
+const aws = require('aws-sdk')
 
 const TMP = './tmp/uploads/'
-const ASSETS = './public/assets/'
 const BUCKET = process.env.BUCKET
 
-let s3 = new aws.S3({apiVersion: '2006-03-01'})
+const s3 = new aws.S3({apiVersion: '2006-03-01'})
 
 module.exports = function (name) {
-  return function (req, res) {
-    new Upload(req, res, req[name]).process()
+  return (req, res) => {
+    new Upload(req.files.image[0], req[name]).process().then(() => {
+      res.flash('Image Uploaded.')
+      res.redirect(req.body.return_to)
+    }).catch(res.error)
   }
 }
 
 class Upload {
 
-  constructor (req, res, model) {
-    this.req = req
-    this.res = res
-    this.file = req.files.image[0]
+  constructor (file, model) {
+    this.file = file
     this.model = model
-    this.path = `${model.tableName}/${model.id}`
   }
 
   s3Key (size) {
-    return `${this.path}/${size}.jpg`
-  }
-
-  assetPath (size) {
-    return `${ASSETS}${this.path}/${size}.jpg`
+    return `${this.model.tableName}/${this.model.id}/${size}.jpg`
   }
 
   tmpPath (size) {
     if (size === 'original') return this.file.path
-    return `${TMP}${size}-${this.file.name}.jpg`
-  }
-
-  error (e) {
-    console.log(e)
-    this.res.status(500).render('500')
+    return `${TMP}${this.file.name}-${size}.jpg`
   }
 
   process () {
-    async.parallel([
-      this.resize.bind(this, 100, 'small'),
-      this.resize.bind(this, 250, 'medium')
-    ], (BUCKET ? this.upload : this.store).bind(this))
+    return Promise.all([
+      this.resize(100, 'small'),
+      this.resize(250, 'medium')
+    ]).then(this.upload.bind(this))
   }
 
-  resize (width, size, next) {
-    gm(this.file.path)
-    .options({imageMagick: true})
-    .resize(width, width)
-    .noProfile()
-    .write(this.tmpPath(size), next)
+  resize (width, size) {
+    return new Promise((resolve, reject) => {
+      gm(this.file.path)
+      .options({imageMagick: true})
+      .resize(width, width)
+      .noProfile()
+      .write(this.tmpPath(size), (e) => { e ? reject(e) : resolve() })
+    })
   }
 
-  upload (e) {
-    if (e) return this.error(e)
-    async.parallel([
-      this.put.bind(this, 'small'),
-      this.put.bind(this, 'medium'),
-      this.put.bind(this, 'original')
-    ], this.remove.bind(this))
+  upload () {
+    return Promise.all([
+      this.put('small'),
+      this.put('medium'),
+      this.put('original')
+    ]).then(this.remove.bind(this))
   }
 
-  remove (e) {
-    if (e) return this.error(e)
-    async.parallel([
-      fs.unlink.bind(fs, this.tmpPath('small')),
-      fs.unlink.bind(fs, this.tmpPath('medium')),
-      fs.unlink.bind(fs, this.tmpPath('original'))
-    ], this.update.bind(this))
+  remove () {
+    return Promise.all([
+      this.rm('small'),
+      this.rm('medium'),
+      this.rm('original')
+    ]).then(this.update.bind(this))
   }
 
-  put (size, next) {
-    s3.putObject({
-      ACL: 'public-read',
-      Body: fs.createReadStream(this.tmpPath(size)),
-      Bucket: BUCKET,
-      CacheControl: 'max-age=' + 60 * 60 * 24 + ', public',
-      ContentType: 'image/jpeg',
-      Key: this.s3Key(size)
-    }, next)
+  rm (size) {
+    return new Promise((resolve, reject) => {
+      fs.unlink(this.tmpPath(size), (e) => { e ? reject(e) : resolve() })
+    })
   }
 
-  store (e) {
-    if (e) return this.error(e)
-    mkdirp(ASSETS + this.path, function (e) {
-      if (e) return this.error(e)
-      async.parallel([
-        this.rename.bind(this, 'small'),
-        this.rename.bind(this, 'medium'),
-        this.rename.bind(this, 'original')
-      ], this.update.bind(this))
-    }.bind(this))
+  put (size) {
+    return new Promise((resolve, reject) => {
+      s3.putObject({
+        ACL: 'public-read',
+        Body: fs.createReadStream(this.tmpPath(size)),
+        Bucket: BUCKET,
+        CacheControl: `max-age=${60 * 60 * 24}, public`,
+        ContentType: 'image/jpeg',
+        Key: this.s3Key(size)
+      }, (e) => { e ? reject(e) : resolve() })
+    })
   }
 
-  rename (size, next) {
-    fs.rename(this.tmpPath(size), this.assetPath(size), next)
-  }
-
-  update (e) {
-    if (e) return this.error(e)
-    this.model.update({imaged_at: new Date()}).then(this.finish.bind(this))
-  }
-
-  finish () {
-    this.res.flash('Image Uploaded.')
-    this.res.redirect(this.req.body.return_to)
+  update () {
+    return this.model.update({imaged_at: new Date()})
   }
 
 }
