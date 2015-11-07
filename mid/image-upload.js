d9f6dd7d1ef51ddde08ee3da4af6a6534783fb8f
Brad Dunbar <dunbarb2@gmail.com>
2015-11-07 17:31:18 -0500

    Improve image uploads.

--- mid/image-upload.js
+++ mid/image-upload.js
@@ -3,10 +3,7 @@
 const fs = require('fs')
 const gm = require('gm')
 const aws = require('aws-sdk')
-
-const TMP = './tmp/uploads/'
 const BUCKET = process.env.BUCKET
-
 const s3 = new aws.S3({apiVersion: '2006-03-01'})
 
 module.exports = function (name) {
@@ -23,6 +20,11 @@ class Upload {
   constructor (file, model) {
     this.file = file
     this.model = model
+    this.sizes = {
+      small: 100,
+      medium: 250,
+      original: null
+    }
   }
 
   s3Key (size) {
@@ -31,46 +33,28 @@ class Upload {
 
   tmpPath (size) {
     if (size === 'original') return this.file.path
-    return `${TMP}${this.file.name}-${size}.jpg`
+    return `tmp/uploads/${this.file.name}-${size}.jpg`
   }
 
   process () {
-    return Promise.all([
-      this.resize(100, 'small'),
-      this.resize(250, 'medium')
-    ]).then(this.upload.bind(this))
+    return Promise.all(
+      Object.keys(this.sizes).map(this.resize.bind(this))
+    ).then(() => {
+      for (let size in this.sizes) fs.unlink(this.tmpPath(size))
+      return this.model.update({imaged_at: new Date()})
+    })
   }
 
-  resize (width, size) {
+  resize (size) {
+    if (size === 'original') return this.put(size)
+    let width = this.sizes[size]
     return new Promise((resolve, reject) => {
       gm(this.file.path)
       .options({imageMagick: true})
       .resize(width, width)
       .noProfile()
-      .write(this.tmpPath(size), (e) => { e ? reject(e) : resolve() })
-    })
-  }
-
-  upload () {
-    return Promise.all([
-      this.put('small'),
-      this.put('medium'),
-      this.put('original')
-    ]).then(this.remove.bind(this))
-  }
-
-  remove () {
-    return Promise.all([
-      this.rm('small'),
-      this.rm('medium'),
-      this.rm('original')
-    ]).then(this.update.bind(this))
-  }
-
-  rm (size) {
-    return new Promise((resolve, reject) => {
-      fs.unlink(this.tmpPath(size), (e) => { e ? reject(e) : resolve() })
-    })
+      .write(this.tmpPath(size), e => e ? reject(e) : resolve())
+    }).then(this.put.bind(this, size))
   }
 
   put (size) {
@@ -82,12 +66,8 @@ class Upload {
         CacheControl: `max-age=${60 * 60 * 24}, public`,
         ContentType: 'image/jpeg',
         Key: this.s3Key(size)
-      }, (e) => { e ? reject(e) : resolve() })
+      }, e => e ? reject(e) : resolve())
     })
-  }
-
-  update () {
-    return this.model.update({imaged_at: new Date()})
   }
 
 }
