ab14737ef48fadc2e7a9c78ba26c99e8952fe3a0
Brad Dunbar <dunbarb2@gmail.com>
2015-11-14 07:13:22 -0500

    Object.assign

--- mid/url.js
+++ mid/url.js
@@ -1,13 +1,11 @@
 'use strict'
 
-let qs = require('qs')
+const qs = require('qs')
 
 module.exports = function (req, res, next) {
   req.urlWith = res.locals.urlWith = function (values) {
-    let query = {}
-    for (let key in req.query) query[key] = req.query[key]
-    for (let key in values) query[key] = values[key]
-    return `${req.baseUrl}${req.path}?${qs.stringify(query)}`
+    let query = qs.stringify(Object.assign({}, req.query, values))
+    return `${req.baseUrl}${req.path}?${query}`
   }
   next()
 }
