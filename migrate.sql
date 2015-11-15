5b9667b8a54e97b15fd70ac9e27a6ca82e36fcd2
Brad Dunbar <dunbarb2@gmail.com>
2015-11-15 08:28:41 -0500

    Drop imaged_at.

--- migrate.sql
+++ migrate.sql
@@ -1,7 +1,3 @@
-alter table growers add column image_updated_at timestamp with time zone;
-alter table products add column image_updated_at timestamp with time zone;
-alter table users add column image_updated_at timestamp with time zone;
-
-update growers set image_updated_at = imaged_at;
-update products set image_updated_at = imaged_at;
-update users set image_updated_at = imaged_at;
+alter table growers drop imaged_at;
+alter table products drop imaged_at;
+alter table users drop imaged_at;
