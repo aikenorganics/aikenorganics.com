8b50c8aa0512ef7c041776414f1c1ddab2ae2f38
Brad Dunbar <dunbarb2@gmail.com>
2015-12-12 08:40:39 -0500

    Simplify

--- test/signin.js
+++ test/signin.js
@@ -1,20 +1,18 @@
 'use strict'
 
-let db = require('../db')
-let Cookie = require('cookies').Cookie
+const db = require('../db')
+const Cookie = require('cookies').Cookie
 
 let users = null
 
 function findUser (email) {
-  return new Promise(function (resolve, reject) {
-    if (users) return resolve(users[email])
-    return db.User.all().then(function (all) {
-      users = all.reduce(function (users, user) {
-        users[user.email] = user
-        return users
-      }, {})
-      return resolve(users[email])
-    }).catch(reject)
+  if (users) return Promise.resolve(users[email])
+  return db.User.all().then((all) => {
+    users = all.reduce((users, user) => {
+      users[user.email] = user
+      return users
+    }, {})
+    return users[email]
   })
 }
 
@@ -22,7 +20,7 @@ module.exports = function (agent, email) {
   return findUser(email).then(function (user) {
     if (!user) return agent
 
-    let value = new Buffer(JSON.stringify({
+    const value = new Buffer(JSON.stringify({
       userId: user.id
     })).toString('base64')
 
