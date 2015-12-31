feea3bc1c2e5ccfd93ec30e3e0075c2f8f4c653f
Brad Dunbar <dunbarb2@gmail.com>
2015-12-31 07:39:40 -0500

    Boomtown.

--- client/views/app.js
+++ client/views/app.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import SubNav from './subnav'
 
-export default function ({cart, market, user, path, children}) {
+export default ({cart, market, user, path, children}) => {
   return <main className='container'>
     <SubNav cart={cart} user={user} open={market.open} path={path}/>
     {children}
