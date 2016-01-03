ee4fead9c55e4c74fdd46623aac3d42cec4b6a4c
Brad Dunbar <dunbarb2@gmail.com>
2016-01-03 08:19:58 -0500

    Just render stuff.

--- client/views/app.js
+++ client/views/app.js
@@ -1,9 +1,52 @@
 import React from 'react'
-import Tabs from './tabs'
 
 export default ({state: {cart, market, user, path}, children}) => {
+
+  const cartSize = Object.keys(cart).reduce((total, id) => total + cart[id], 0)
+
   return <main className='container'>
-    <Tabs cart={cart} user={user} open={market.open} path={path}/>
+    <div className='hidden-print'>
+      {market.open ?
+        <div className='alert alert-info'>
+          <p>
+            <strong>Hi!</strong> Aiken Organics is still under development,
+            so please bear with us while we work out the kinks!
+            If something goes wrong, or you have a question, or you just want to
+            shoot the breeze, send an email to <a href='mailto:support@aikenorganics.com'>support@aikenorganics.com</a>.
+          </p>
+        </div>
+        :
+        <div className='alert alert-warning text-center'>
+          <p>
+            <strong>The market is currently closed and will re-open on Sunday at 8:00AM. </strong>
+            If you need help in the meantime, feel free to drop us a line
+            at <a href='mailto:support@aikenorganics.com'>support@aikenorganics.com</a>.
+          </p>
+        </div>
+      }
+      <ul className='nav nav-tabs subnav'>
+        <li className={/^\/products/.test(path) ? 'active' : ''}>
+          <a href='/products'>Products</a>
+        </li>
+        <li className={/^\/growers/.test(path) ? 'active' : ''}>
+          <a href='/growers'>Growers</a>
+        </li>
+        {!user || !market.open ? '' :
+          <li className={/^\/cart/.test(path) ? 'active' : ''}>
+            <a href='/cart'>
+              Cart <span id='cart-size' className='badge'>{cartSize}</span>
+            </a>
+          </li>
+        }
+        {!user ? '' :
+          <li className={/^\/orders/.test(path) ? 'active' : ''}>
+            <a href='/orders/current'>
+              Orders
+            </a>
+          </li>
+        }
+      </ul>
+    </div>
     {children}
   </main>
 }
