69a2fe02d3bf19b992aaaedeee97130089d3be67
Brad Dunbar <dunbarb2@gmail.com>
2016-01-30 10:12:17 -0500

    Unrecognized email.

--- test/integration/index.js
+++ test/integration/index.js
@@ -16,3 +16,18 @@ test('sign in', (t) => {
     t.end()
   })
 })
+
+test('email not found', (t) => {
+  t.visit('/auth/signin')
+  t.$('[name=email]').sendKeys('nobody@example.com')
+  t.$('[name=password]').sendKeys('password' + t.Key.RETURN)
+  t.until.stalenessOf(t.$('html'))
+  Promise.all([
+    t.getPath(),
+    t.$('#error').getText()
+  ]).then((results) => {
+    t.is(results[0], '/auth/signin')
+    t.is(results[1], 'Sorry! We don’t recognize that email.')
+    t.end()
+  })
+})
