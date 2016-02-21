c8ff46082e6eb6ecc4f6fb147d4bb4e86f40f1a0
Brad Dunbar <dunbarb2@gmail.com>
2016-02-21 10:01:43 -0500

    Syntax upgrade.

--- routes/orders.js
+++ routes/orders.js
@@ -1,10 +1,9 @@
 'use strict'
 
-let db = require('../db')
-let ozymandias = require('ozymandias')
-let router = module.exports = ozymandias.Router()
+const db = require('../db')
+const router = module.exports = require('ozymandias').Router()
 
-router.use(function (req, res, next) {
+router.use((req, res, next) => {
   if (req.user) return next()
   res.status(401).render('401')
 })
@@ -12,14 +11,14 @@ router.use(function (req, res, next) {
 router.find('order', () => db.Order)
 
 // Current
-router.get('/current', function (req, res) {
+router.get('/current', (req, res) => {
   Promise.all([
     db.Order
       .include('location', {productOrders: 'product'})
       .where({status: 'open', user_id: req.user.id})
       .find(),
     db.Location.where({active: true}).order('name').all()
-  ]).then(function (results) {
+  ]).then((results) => {
     res.render('orders/current', {
       order: results[0],
       locations: results[1]
@@ -28,7 +27,7 @@ router.get('/current', function (req, res) {
 })
 
 // Update
-router.post('/:order_id', function (req, res) {
+router.post('/:order_id', (req, res) => {
   if (!req.market.open) return res.status(401).render('401')
 
   // You can only update your own order.
@@ -36,14 +35,14 @@ router.post('/:order_id', function (req, res) {
     return res.status(401).render('401')
   }
 
-  req.order.update(req.permit('location_id')).then(function () {
+  req.order.update(req.permit('location_id')).then(() => {
     res.flash('success', 'Saved')
     res.redirect('/orders/current')
   }).catch(res.error)
 })
 
 // Cancel
-router.post('/:order_id/cancel', function (req, res) {
+router.post('/:order_id/cancel', (req, res) => {
   if (!req.market.open) return res.status(401).render('401')
 
   // You can only cancel your own order.
@@ -51,7 +50,7 @@ router.post('/:order_id/cancel', function (req, res) {
     return res.status(401).render('401')
   }
 
-  req.order.destroy().then(function () {
+  req.order.destroy().then(() => {
     res.flash('success', 'Order cancelled.')
     res.redirect('/products')
   }).catch(res.error)
