f99858f82078a166d7e2410f867cce01df019f89
Brad Dunbar <dunbarb2@gmail.com>
2016-03-12 08:09:06 -0500

    Syntax.

--- routes/admin/categories.js
+++ routes/admin/categories.js
@@ -1,48 +1,47 @@
 'use strict'
 
-let ozymandias = require('ozymandias')
-let db = require('../../db')
-let router = module.exports = ozymandias.Router()
+const db = require('../../db')
+const router = module.exports = require('ozymandias').Router()
 
 // Find
 router.find('category', () => db.Category)
 
 // Index
-router.get('/', function (req, res) {
-  db.Category.order('position').all().then(function (categories) {
+router.get('/',  (req, res) => {
+  db.Category.order('position').all().then((categories) => {
     res.render('admin/categories/index', {categories: categories})
   })
 })
 
 // New
-router.get('/new', function (req, res) {
+router.get('/new', (req, res) => {
   res.render('admin/categories/new')
 })
 
 // Edit
-router.get('/:category_id/edit', function (req, res) {
+router.get('/:category_id/edit', (req, res) => {
   res.render('admin/categories/edit')
 })
 
 // Create
-router.post('/', function (req, res) {
-  db.Category.create(req.permit('name', 'position')).then(function () {
+router.post('/', (req, res) => {
+  db.Category.create(req.permit('name', 'position')).then(() => {
     res.flash('success', 'Created')
     res.redirect('/admin/categories')
   })
 })
 
 // Update
-router.post('/:category_id', function (req, res) {
-  req.category.update(req.permit('name', 'position')).then(function () {
+router.post('/:category_id', (req, res) => {
+  req.category.update(req.permit('name', 'position')).then(() => {
     res.flash('success', 'Saved')
     res.redirect('/admin/categories')
   })
 })
 
 // Delete
-router.post('/:category_id/delete', function (req, res) {
-  req.category.destroy().then(function () {
+router.post('/:category_id/delete', (req, res) => {
+  req.category.destroy().then(() => {
     res.flash('success', 'Deleted')
     res.redirect('/admin/categories')
   })
