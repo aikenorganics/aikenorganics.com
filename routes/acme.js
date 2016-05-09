cbd215e820f1181f952bf2c878e3670a822fc69c
Brad Dunbar <dunbarb2@gmail.com>
2016-05-09 09:13:03 -0400

    Acme challenge.

--- routes/acme.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const router = module.exports = require('ozymandias').Router()
-
-router.get('/l8vzbBXaCWcmA9vXrGwi44Iz1IX5PmBm53Wity1BbyE', (req, res) => {
-  res.set('Content-Type', 'text/plain')
-  res.send('l8vzbBXaCWcmA9vXrGwi44Iz1IX5PmBm53Wity1BbyE.ceWqF_4Sc8ECFdbNG-K06eUghznPWnSMud9vYlAafe0')
-  res.end()
-})
