73267c58a8479d5bb25a81918aa88684c48eabfa
Brad Dunbar <dunbarb2@gmail.com>
2016-07-20 10:49:23 -0400

    Clean up.

--- client/index.js
+++ client/index.js
@@ -1,6 +1,35 @@
 import './error-tracking'
-import './pages/index'
+import React from 'react'
+import {render} from 'react-dom'
+import store from './store'
+import Routes from './routes'
+import {navigate} from './actions/index'
 
 // Bootstrap relies on window.jQuery
 window.jQuery = require('jquery')
 require('bootstrap')
+
+// The element to render components into.
+const root = document.getElementById('root')
+
+// Update the DOM
+const update = () => render(<Routes {...store.getState()}/>, root)
+
+// Render when the store is updated.
+store.subscribe(update)
+
+// Navigate on popstate.
+let hashChanged
+window.addEventListener('popstate', () => {
+  hashChanged = false
+  setTimeout(() => {
+    if (!hashChanged) navigate(window.location.href, {push: false})
+  }, 0)
+})
+
+window.addEventListener('hashchange', () => {
+  hashChanged = true
+})
+
+// Kick the tires
+if (root) update()
