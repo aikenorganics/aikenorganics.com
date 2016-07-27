228a43ee02aaa8e286ebe567271412ca56c66309
Brad Dunbar <dunbarb2@gmail.com>
2016-07-27 17:55:09 -0400

    Change location without save button.

--- client/views/orders/form.js
+++ client/views/orders/form.js
@@ -10,15 +10,11 @@ export default class Form extends Component {
     this.state = {location_id}
   }
 
-  save (e) {
-    if (e) e.preventDefault()
-    const {busy, order} = this.props
-    if (busy) return
-    updateOrder(order.id, this.state).catch((e) => {})
-  }
-
   setLocation (e) {
-    this.setState({location_id: +e.target.value || null})
+    const {id} = this.props.order
+    const location_id = +e.target.value || null
+    this.setState({location_id})
+    updateOrder(id, {location_id}).catch((e) => {})
   }
 
   render () {
@@ -26,32 +22,25 @@ export default class Form extends Component {
     const {address, canDeliver} = this.props.user
     const {location_id} = this.state
 
-    return <form onSubmit={(e) => this.save(e)}>
-      <div className='form-group'>
-        <label>Pickup Location / Delivery</label>
-        <select className='form-control' value={location_id || ''} onChange={(e) => this.setLocation(e)}>
-          <option value='0' disabled={!canDeliver}>
-            {canDeliver
-              ? `Deliver to ${address}`
-              : 'Delivery - Please update your settings first.'
-            }
-          </option>
-          {locations.map(({id, name}) => <option key={id} value={id}>{name}</option>)}
-        </select>
-        {!canDeliver
-          ? <p className='help-block'>
-            For delivery, please enter your address, phone number, and
-            billing information. <Link href='/settings'>Edit Settings</Link>
-          </p>
-          : ''
-        }
-      </div>
-      <div className='text-right'>
-        <button className='btn btn-success' disabled={busy}>
-          Update Order
-        </button>
-      </div>
-    </form>
+    return <div className='form-group'>
+      <label>Pickup Location / Delivery</label>
+      <select className='form-control' value={location_id || ''} onChange={(e) => this.setLocation(e)} disabled={busy}>
+        <option value='0' disabled={!canDeliver}>
+          {canDeliver
+            ? `Deliver to ${address}`
+            : 'Delivery - Please update your settings first.'
+          }
+        </option>
+        {locations.map(({id, name}) => <option key={id} value={id}>{name}</option>)}
+      </select>
+      {!canDeliver
+        ? <p className='help-block'>
+          For delivery, please enter your address, phone number, and
+          billing information. <Link href='/settings'>Edit Settings</Link>
+        </p>
+        : ''
+      }
+    </div>
   }
 
 }
