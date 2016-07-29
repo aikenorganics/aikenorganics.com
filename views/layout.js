16ab6f632121092f835732b7e7a25b2df77b728f
Brad Dunbar <dunbarb2@gmail.com>
2016-07-28 20:17:01 -0400

    Formatting

--- views/layout.js
+++ views/layout.js
@@ -5,15 +5,14 @@ const {html, raw} = require('ozymandias/html')
 
 const analytics = () => {
   if (process.env.NODE_ENV !== 'production') return ''
-  return html`
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-  ga('create', 'UA-60091047-1', 'auto');
-  ga('send', 'pageview');
-</script>`
+  return html`<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga')
+    ga('create','UA-60091047-1','auto')
+    ga('send','pageview')
+  </script>`
 }
 
 module.exports = ({json, state}, content) => html`
