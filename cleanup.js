dd4668d37a89d4637c579b459b9ffc35ecb0f482
Brad Dunbar <dunbarb2@gmail.com>
2016-07-30 10:29:10 -0400

    More cleanup.

--- cleanup.js
+++ cleanup.js
@@ -6,23 +6,15 @@ const aws = require('aws-sdk')
 const s3 = new aws.S3({apiVersion: '2006-03-01'})
 const Bucket = process.env.BUCKET
 
-const all = () => (
-  new Promise((resolve, reject) => {
-    let all = []
-    const fetch = (ContinuationToken) => {
-      s3.listObjectsV2({Bucket, ContinuationToken}, (error, data) => {
-        if (error) return reject(error)
-        all = all.concat(data)
-        if (data.IsTruncated) fetch(data.NextContinuationToken)
-        else resolve(all)
-      })
-    }
-    fetch()
+const clean = (ContinuationToken) => {
+  s3.listObjectsV2({Bucket, ContinuationToken}, (error, data) => {
+    if (error) return console.log(error)
+    const keys = data.Contents
+      .map(({Key}) => Key)
+      .filter((Key) => !/(small|medium|original)$/.test(Key))
+    console.log(keys)
+    if (data.IsTruncated) clean(data.NextContinuationToken)
   })
-)
+}
 
-all().then((objects) => {
-  console.log(objects)
-}).catch((error) => {
-  console.log(error)
-})
+clean()
