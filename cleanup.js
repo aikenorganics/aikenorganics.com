bf52c817d02d01bf108dbac94680977ba3a34440
Brad Dunbar <dunbarb2@gmail.com>
2016-07-30 10:36:22 -0400

    CLEAN UP

--- cleanup.js
+++ cleanup.js
@@ -7,13 +7,21 @@ const s3 = new aws.S3({apiVersion: '2006-03-01'})
 const Bucket = process.env.BUCKET
 
 const clean = (ContinuationToken) => {
-  s3.listObjectsV2({Bucket, ContinuationToken}, (error, data) => {
+  s3.listObjectsV2({
+    Bucket,
+    ContinuationToken
+  }, (error, {Contents, IsTruncated, NextContinuationToken}) => {
     if (error) return console.log(error)
-    const keys = data.Contents
+    const keys = Contents
       .map(({Key}) => Key)
       .filter((Key) => !/(small|medium|original)$/.test(Key))
-    console.log(keys)
-    if (data.IsTruncated) clean(data.NextContinuationToken)
+    s3.deleteObjects({
+      Bucket,
+      Delete: {Objects: keys.map((Key) => ({Key}))}
+    }, (error) => {
+      if (error) return console.log(error)
+      if (IsTruncated) clean(NextContinuationToken)
+    })
   })
 }
 
