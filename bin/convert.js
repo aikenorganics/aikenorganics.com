d0c8b4ba8fb29f38ca304d40ed05558867ebdf0f
Brad Dunbar <dunbarb2@gmail.com>
2016-09-28 23:36:36 -0400

    Clean up convert.js

--- bin/convert.js
+++ bin/convert.js
@@ -3,20 +3,29 @@
 'use strict'
 
 const db = require('../db')
+const {Grower, Product, User} = db
 
-Promise.all([
-  db.Grower.not({imageUpdatedAt: null}).all(),
-  db.Product.not({imageUpdatedAt: null}).all(),
-  db.User.not({imageUpdatedAt: null}).all()
-]).then(([growers, products, users]) => {
-  const models = [].concat(growers, products, users)
-  const convert = () => {
+const queue = (job, concurrency = 1) => {
+  const enqueue = () => {
+    const result = job()
+    if (result) return result.then(enqueue)
+  }
+
+  const workers = []
+  for (let i = 0; i < concurrency; i++) workers.push(enqueue())
+  return Promise.all(workers)
+}
+
+Promise.all([Grower, Product, User].map((Model) =>
+  Model.not({imageUpdatedAt: null}).all()
+)).then((result) => {
+  const models = [].concat(...result)
+  return queue(() => {
     if (!models.length) return
     const model = models.pop()
     console.log(`Converting ${model.tableName}:${model.id}`)
-    return model.convertImage().then(() => convert())
-  }
-  return Promise.all([convert(), convert(), convert()])
+    return model.convertImage()
+  }, 3)
 })
 .then(() => db.close())
 .catch((error) => console.log(error))
