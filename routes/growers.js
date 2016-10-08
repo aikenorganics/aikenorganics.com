43ce050880266d80494bf8beb1bcc0991d956d03
Brad Dunbar <dunbarb2@gmail.com>
2016-10-08 17:36:17 -0400

    Remove mid/growers/authorize

--- routes/growers.js
+++ routes/growers.js
@@ -8,7 +8,16 @@ const router = module.exports = require('ozymandias').Router()
 router.find('grower', () => db.Grower)
 
 // Authorize
-router.param('growerId', require('../mid/growers/authorize'))
+router.param('growerId', (req, res, next) => {
+  if (!req.currentUser || !req.grower) return next()
+  db.UserGrower.where({
+    userId: req.currentUser.id,
+    growerId: req.grower.id
+  }).find().then((userGrower) => {
+    req.canEdit = res.locals.canEdit = req.admin || !!userGrower
+    next()
+  }).catch(res.error)
+})
 
 // Index
 router.get('/', (req, res) => {
--- test/mid/index.js
+++ test/mid/index.js
@@ -1,2 +1,1 @@
-require('./growers/authorize')
 require('./products/authorize')
