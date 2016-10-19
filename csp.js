f26f48f8397ff32fdff479cd20dbe39179918c71
Brad Dunbar <dunbarb2@gmail.com>
2016-10-18 22:23:37 -0400

    node 6.9.0

--- csp.js
+++ csp.js
@@ -2,8 +2,8 @@
 
 module.exports = function *(next) {
   this.csp('img-src', 'https://q.stripe.com')
-  this.csp('frame-src', "https://checkout.stripe.com")
+  this.csp('frame-src', 'https://checkout.stripe.com')
   this.csp('script-src', 'https://checkout.stripe.com')
-  this.csp('connect-src', "https://checkout.stripe.com")
+  this.csp('connect-src', 'https://checkout.stripe.com')
   yield next
 }
