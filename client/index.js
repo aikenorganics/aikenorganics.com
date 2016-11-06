cd32fa5ef9b7f39c5ea9346f7939b6d7aa34a356
Brad Dunbar <dunbarb2@gmail.com>
2016-11-06 11:09:32 -0500

    Drop popstate navigation.

--- client/index.js
+++ client/index.js
@@ -5,7 +5,6 @@ import React from 'react'
 import store from './store'
 import Routes from './routes'
 import {render} from 'react-dom'
-import {navigate} from './actions'
 
 // The element to render components into.
 const root = document.getElementById('root')
@@ -15,15 +14,6 @@ const update = () => render(<Routes {...store.getState()} />, root)
 
 // Render when the store is updated.
 store.subscribe(update)
-
-// Navigate on popstate.
-window.addEventListener('popstate', () => {
-  // The popstate event is also fired on hashchange, so make sure the path has
-  // actually changed!
-  if (store.getState().path !== window.location.pathname) {
-    navigate(window.location.href, {push: false})
-  }
-})
 
 // Kick the tires
 if (root) update()
