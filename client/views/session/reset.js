3ed3852edcd1d198ba77acfaab63c8f9f0839bcb
Brad Dunbar <dunbarb2@gmail.com>
2016-11-06 11:54:00 -0500

    Reset view

--- client/views/session/reset.js
+++ client/views/session/reset.js
@@ -1,45 +1,12 @@
 import React from 'react'
-import Link from '../link'
-import Errors from '../errors'
-import {navigate, reset} from '../../actions'
+import Reset from 'ozymandias/client/views/reset'
 
-export default ({busy, email, errors, expired, token}) => {
-  let passwordInput
-
-  const submit = (event) => {
-    event.preventDefault()
-    reset(token, {password: passwordInput.value}).then(() => {
-      navigate('/products')
-    }).catch(() => {})
-  }
-
+export default (props) => {
   return <main className='container'>
     <div className='row'>
-      <form onSubmit={submit} className='col-md-6 offset-md-3'>
-        <h3>Reset Password</h3>
-        {expired
-          ? <div>
-            <h3>Sorry! That token is expired.</h3>
-            <p>
-              Would you like us to <Link href='/signin/forgot'>send you another one</Link>?
-            </p>
-          </div>
-          : <div className='form-group'>
-            <label htmlFor='password'>New Password for {email || ''}</label>
-            <input type='password' id='password' className='form-control'
-              required autoFocus ref={(input) => { passwordInput = input }} />
-          </div>
-        }
-        <Errors errors={errors} />
-        {token
-          ? <div className='text-xs-right'>
-            <button className='btn btn-success' disabled={busy}>
-              Reset Password
-            </button>
-          </div>
-          : ''
-        }
-      </form>
+      <div className='col-md-6 offset-md-3'>
+        <Reset {...props} returnTo='/products' />
+      </div>
     </div>
   </main>
 }
