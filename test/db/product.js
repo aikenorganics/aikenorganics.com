31ea6d27d532503d43d23d6b1bd402518a30fd96
Brad Dunbar <dunbarb2@gmail.com>
2017-03-20 09:08:45 -0400

    s-t-assert-

--- test/db/product.js
+++ test/db/product.js
@@ -3,97 +3,97 @@
 const test = require('../test')
 const {Product} = require('../../db')
 
-test('Product#available subtracts reserved from supply', async (t) => {
+test('Product#available subtracts reserved from supply', async (assert) => {
   const product = new Product({
     supply: 100,
     reserved: 15
   })
-  t.equal(product.available, 85)
+  assert.equal(product.available, 85)
   product.reserved = 112
-  t.equal(product.available, 0)
+  assert.equal(product.available, 0)
 })
 
-test('oversold', async (t) => {
+test('oversold', async (assert) => {
   const product = new Product({
     supply: 100,
     reserved: 15
   })
-  t.ok(!product.oversold)
+  assert.ok(!product.oversold)
   product.supply = 10
-  t.ok(product.oversold)
+  assert.ok(product.oversold)
 })
 
-test('Product#reservedCost', async (t) => {
+test('Product#reservedCost', async (assert) => {
   const product = new Product({
     supply: 100,
     reserved: 8,
     cost: '1.25'
   })
-  t.equal(product.reservedCost(), 10)
+  assert.equal(product.reservedCost(), 10)
 })
 
-test('validate cost', async (t) => {
+test('validate cost', async (assert) => {
   const product = new Product({cost: 'asdf'})
-  t.ok(!product.valid)
-  t.deepEqual(product.errors.cost, ['Cost must be a valid dollar amount'])
+  assert.ok(!product.valid)
+  assert.deepEqual(product.errors.cost, ['Cost must be a valid dollar amount'])
 })
 
-test('validate cost', async (t) => {
+test('validate cost', async (assert) => {
   const product = new Product({cost: '.53'})
   product.validate()
-  t.ok(!product.errors.cost)
+  assert.ok(!product.errors.cost)
 })
 
-test('validate cost', async (t) => {
+test('validate cost', async (assert) => {
   const product = new Product({cost: '32'})
   product.validate()
-  t.ok(!product.errors.cost)
+  assert.ok(!product.errors.cost)
 })
 
-test('validate cost', async (t) => {
+test('validate cost', async (assert) => {
   const product = new Product({cost: '32.25'})
   product.validate()
-  t.ok(!product.errors.cost)
+  assert.ok(!product.errors.cost)
 })
 
-test('validate cost', async (t) => {
+test('validate cost', async (assert) => {
   const product = new Product({cost: '32.25'})
   product.validate()
-  t.ok(!product.errors.cost)
+  assert.ok(!product.errors.cost)
 })
 
-test('validate cost', async (t) => {
+test('validate cost', async (assert) => {
   const product = new Product({cost: '  10  '})
   product.validate()
-  t.ok(!product.errors.cost)
+  assert.ok(!product.errors.cost)
 })
 
-test('validate cost', async (t) => {
+test('validate cost', async (assert) => {
   const product = new Product({cost: '  $32.25  '})
   product.validate()
-  t.ok(!product.errors.cost)
+  assert.ok(!product.errors.cost)
 })
 
-test('validate name', async (t) => {
+test('validate name', async (assert) => {
   const product = new Product({name: ''})
   product.validate()
-  t.deepEqual(product.errors.name, ['Name cannot be blank'])
+  assert.deepEqual(product.errors.name, ['Name cannot be blank'])
 })
 
-test('validate supply', async (t) => {
+test('validate supply', async (assert) => {
   const product = new Product({supply: -2})
   product.validate()
-  t.deepEqual(product.errors.supply, ['Supply cannot be negative'])
+  assert.deepEqual(product.errors.supply, ['Supply cannot be negative'])
 })
 
-test('non-numeric supply', async (t) => {
+test('non-numeric supply', async (assert) => {
   const product = new Product({supply: 'asdf'})
   product.validate()
-  t.deepEqual(product.errors.supply, ['Supply must be a number'])
+  assert.deepEqual(product.errors.supply, ['Supply must be a number'])
 })
 
-test('empty supply', async (t) => {
+test('empty supply', async (assert) => {
   const product = new Product({supply: ''})
   product.validate()
-  t.deepEqual(product.errors.supply, ['Supply must be a number'])
+  assert.deepEqual(product.errors.supply, ['Supply must be a number'])
 })
