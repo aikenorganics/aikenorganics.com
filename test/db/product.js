ceee8bc43405ccc57213b4ada6e3dd636c773379
Brad Dunbar <dunbarb2@gmail.com>
2017-03-20 09:08:08 -0400

    clean up

--- test/db/product.js
+++ test/db/product.js
@@ -1,10 +1,10 @@
 'use strict'
 
 const test = require('../test')
-const db = require('../../db')
+const {Product} = require('../../db')
 
 test('Product#available subtracts reserved from supply', async (t) => {
-  const product = new db.Product({
+  const product = new Product({
     supply: 100,
     reserved: 15
   })
@@ -14,7 +14,7 @@ test('Product#available subtracts reserved from supply', async (t) => {
 })
 
 test('oversold', async (t) => {
-  const product = new db.Product({
+  const product = new Product({
     supply: 100,
     reserved: 15
   })
@@ -24,7 +24,7 @@ test('oversold', async (t) => {
 })
 
 test('Product#reservedCost', async (t) => {
-  const product = new db.Product({
+  const product = new Product({
     supply: 100,
     reserved: 8,
     cost: '1.25'
@@ -33,67 +33,67 @@ test('Product#reservedCost', async (t) => {
 })
 
 test('validate cost', async (t) => {
-  const product = new db.Product({cost: 'asdf'})
+  const product = new Product({cost: 'asdf'})
   t.ok(!product.valid)
   t.deepEqual(product.errors.cost, ['Cost must be a valid dollar amount'])
 })
 
 test('validate cost', async (t) => {
-  const product = new db.Product({cost: '.53'})
+  const product = new Product({cost: '.53'})
   product.validate()
   t.ok(!product.errors.cost)
 })
 
 test('validate cost', async (t) => {
-  const product = new db.Product({cost: '32'})
+  const product = new Product({cost: '32'})
   product.validate()
   t.ok(!product.errors.cost)
 })
 
 test('validate cost', async (t) => {
-  const product = new db.Product({cost: '32.25'})
+  const product = new Product({cost: '32.25'})
   product.validate()
   t.ok(!product.errors.cost)
 })
 
 test('validate cost', async (t) => {
-  const product = new db.Product({cost: '32.25'})
+  const product = new Product({cost: '32.25'})
   product.validate()
   t.ok(!product.errors.cost)
 })
 
 test('validate cost', async (t) => {
-  const product = new db.Product({cost: '  10  '})
+  const product = new Product({cost: '  10  '})
   product.validate()
   t.ok(!product.errors.cost)
 })
 
 test('validate cost', async (t) => {
-  const product = new db.Product({cost: '  $32.25  '})
+  const product = new Product({cost: '  $32.25  '})
   product.validate()
   t.ok(!product.errors.cost)
 })
 
 test('validate name', async (t) => {
-  const product = new db.Product({name: ''})
+  const product = new Product({name: ''})
   product.validate()
   t.deepEqual(product.errors.name, ['Name cannot be blank'])
 })
 
 test('validate supply', async (t) => {
-  const product = new db.Product({supply: -2})
+  const product = new Product({supply: -2})
   product.validate()
   t.deepEqual(product.errors.supply, ['Supply cannot be negative'])
 })
 
 test('non-numeric supply', async (t) => {
-  const product = new db.Product({supply: 'asdf'})
+  const product = new Product({supply: 'asdf'})
   product.validate()
   t.deepEqual(product.errors.supply, ['Supply must be a number'])
 })
 
 test('empty supply', async (t) => {
-  const product = new db.Product({supply: ''})
+  const product = new Product({supply: ''})
   product.validate()
   t.deepEqual(product.errors.supply, ['Supply must be a number'])
 })
