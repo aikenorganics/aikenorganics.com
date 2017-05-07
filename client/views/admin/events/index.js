b9a05af890752675cf31f8d709e68735a74a6d37
Brad Dunbar <dunbarb2@gmail.com>
2017-05-07 18:39:04 -0400

    /admin/events uses URL

--- client/views/admin/events/index.js
+++ client/views/admin/events/index.js
@@ -2,11 +2,14 @@ import React from 'react'
 import Event from './event'
 import Pagination from '../../pagination'
 import {navigate} from '../../../actions'
-import {params} from '../../../url'
 
 export default ({events, more, page, url, userId, users}) => {
-  const changeUser = (event) => {
-    navigate(params(url, {page: null, userId: event.target.value || null}))
+  const changeUser = ({target}) => {
+    const next = new window.URL(url)
+    next.searchParams.delete('page')
+    if (!target.value) next.searchParams.delete('userId')
+    else next.searchParams.set('userId', target.value)
+    navigate(next.toString())
   }
 
   return <div>
--- test/integration/admin/index.js
+++ test/integration/admin/index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 require('./categories')
+require('./events')
 require('./locations')
 require('./market')
 require('./orders')
